fix bug in odometry calculation and add reward specific to foot placement

source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/hector/mdp/observation/observations.py
@@ -25,7 +25,10 @@ if TYPE_CHECKING:
     from isaaclab.envs import ManagerBasedEnv, ManagerBasedRLEnv
     
     
-    
+"""
+General
+"""
+
 def joint_pos(env: ManagerBasedEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"), joint_names: list[str]=[]) -> torch.Tensor:
     """The joint positions of the asset.
 
@@ -34,7 +37,14 @@ def joint_pos(env: ManagerBasedEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("
     # extract the used quantities (to enable type-hinting)
     asset: Articulation = env.scene[asset_cfg.name]
     joint_ids, _ = asset.find_joints(joint_names, preserve_order=True)
-    return asset.data.joint_pos[:, joint_ids]
+    joint_pos = asset.data.joint_pos[:, joint_ids]
+    joint_pos[:, 2] += torch.pi/4
+    joint_pos[:, 3] -= torch.pi/2
+    joint_pos[:, 4] += torch.pi/4
+    joint_pos[:, 7] += torch.pi/4
+    joint_pos[:, 8] -= torch.pi/2
+    joint_pos[:, 9] += torch.pi/4
+    return joint_pos
 
 def joint_vel(env: ManagerBasedEnv, asset_cfg: SceneEntityCfg = SceneEntityCfg("robot"), joint_names: list[str]=[]) -> torch.Tensor:
     """The joint velocities of the asset.
@@ -65,3 +75,48 @@ def height_scan(env: ManagerBasedEnv, sensor_cfg: SceneEntityCfg) -> torch.Tenso
     # extract the used quantities (to enable type-hinting)
     sensor: RayCaster = env.scene.sensors[sensor_cfg.name]
     return sensor.data.ray_hits_w[..., 2]  - sensor.data.pos_w[:, 2].unsqueeze(1)
+
+
+"""
+mpc states
+"""
+
+def swing_phase(env: ManagerBasedEnv, action_name: str = "mpc_action")-> torch.Tensor:
+    """Swing phase of the robot.
+
+    The swing phase is defined as the phase where the foot is not in contact with the ground.
+    """
+    # extract the used quantities (to enable type-hinting)
+    action_term = env.action_manager.get_term(action_name)
+    swing_phase = action_term.swing_phase
+    return swing_phase
+
+def foot_placement_b(env: ManagerBasedEnv, action_name: str = "mpc_action") -> torch.Tensor:
+    """Foot placement of the robot.
+
+    The foot placement is defined as the position of the foot in the world frame.
+    """
+    # extract the used quantities (to enable type-hinting)
+    action_term = env.action_manager.get_term(action_name)
+    foot_placement_b = action_term.foot_placement_b
+    return foot_placement_b
+
+def foot_position_b(env: ManagerBasedEnv, action_name: str = "mpc_action") -> torch.Tensor:
+    """Foot position of the robot.
+
+    The foot position is defined as the position of the foot in the world frame.
+    """
+    # extract the used quantities (to enable type-hinting)
+    action_term = env.action_manager.get_term(action_name)
+    foot_position_b = action_term.foot_pos_b
+    return foot_position_b
+
+def reference_foot_position_b(env: ManagerBasedEnv, action_name: str = "mpc_action") -> torch.Tensor:
+    """Reference foot position of the robot.
+
+    The reference foot position is defined as the position of the foot in the world frame.
+    """
+    # extract the used quantities (to enable type-hinting)
+    action_term = env.action_manager.get_term(action_name)
+    reference_foot_position_b = action_term.ref_foot_pos_b
+    return reference_foot_position_b