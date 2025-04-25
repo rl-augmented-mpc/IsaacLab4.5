fix bug in mpc reset (wrong index)

source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/hector/mdp/actions/mpc_actions.py
@@ -175,7 +175,6 @@ class MPCAction(ActionTerm):
         px = (swing_foot_pos[:, 0]//scan_resolution).long() + int(scan_width//scan_resolution+1)/2
         py = -(swing_foot_pos[:, 1]//scan_resolution).long() + int(scan_width//scan_resolution+1)/2
         indices = (int(scan_width/scan_resolution + 1)*px + py).long()
-        # print(indices.shape)
         self.reference_height = self.cfg.reference_height + height_map[torch.arange(self.num_envs), indices].cpu().numpy() # type: ignore
 
     def apply_actions(self):
@@ -262,12 +261,12 @@ class MPCAction(ActionTerm):
         self.foot_placement_b[:, 3] = -self.foot_placement_w[:, 2]*torch.sin(self.root_yaw) + self.foot_placement_w[:, 3]*torch.cos(self.root_yaw) - self.root_pos[:, 1]
         
         # body-leg angle
-        stance_leg_r_left = torch.abs(self.foot_pos_b[:, :3])
-        stance_leg_r_right = torch.abs(self.foot_pos_b[:, 3:])
-        self.leg_angle[:, 0] = torch.abs(torch.atan2(stance_leg_r_left[:, 0], self.root_pos[:, 2]))
-        self.leg_angle[:, 1] = torch.abs(torch.atan2(stance_leg_r_left[:, 1], self.root_pos[:, 2]))
-        self.leg_angle[:, 2] = torch.abs(torch.atan2(stance_leg_r_right[:, 0], self.root_pos[:, 2]))
-        self.leg_angle[:, 3] = torch.abs(torch.atan2(stance_leg_r_right[:, 1], self.root_pos[:, 2]))
+        stance_leg_r_left = torch.abs(self.foot_pos_b[:, :3]).clamp(min=1e-6)  # avoid division by zero
+        stance_leg_r_right = torch.abs(self.foot_pos_b[:, 3:]).clamp(min=1e-6)  # avoid division by zero
+        self.leg_angle[:, 0] = torch.abs(torch.atan2(stance_leg_r_left[:, 0], stance_leg_r_left[:, 2]))
+        self.leg_angle[:, 1] = torch.abs(torch.atan2(stance_leg_r_left[:, 1], stance_leg_r_left[:, 2]))
+        self.leg_angle[:, 2] = torch.abs(torch.atan2(stance_leg_r_right[:, 0], stance_leg_r_right[:, 2]))
+        self.leg_angle[:, 3] = torch.abs(torch.atan2(stance_leg_r_right[:, 1], stance_leg_r_right[:, 2]))
     
     def _add_joint_offset(self, joint_pos:torch.Tensor) -> torch.Tensor:
         joint_pos[:, 2] += torch.pi/4
@@ -287,5 +286,8 @@ class MPCAction(ActionTerm):
         self._raw_actions[env_ids] = 0.0
         self._processed_actions[env_ids] = 0.0
         # reset mpc controller
-        for i in range(self.num_envs):
+        for i in env_ids.cpu().numpy(): # type: ignore
             self.mpc_controller[i].reset()
+            self.mpc_controller[i].update_gait_parameter(
+                np.array([int(self.cfg.double_support_duration/self._env.physics_dt), int(self.cfg.double_support_duration/self._env.physics_dt)]), 
+                np.array([int(self.cfg.single_support_duration/self._env.physics_dt), int(self.cfg.single_support_duration/self._env.physics_dt)]),)

source/isaaclab_tasks/isaaclab_tasks/manager_based/locomotion/velocity/config/hector/mdp/reward/rewards.py
@@ -35,34 +35,43 @@ def get_ground_gradient(height_map_2d:torch.Tensor)->torch.Tensor:
 
 def get_ground_gradient_at_landing_point(
     num_envs: int,
-    foot_placement: torch.Tensor, 
-    gait_contact: torch.Tensor, 
+    foot_placement: torch.Tensor,
+    gait_contact: torch.Tensor,
     height_map_2d: torch.Tensor,
     ):
-    
     foot_placement = foot_placement.reshape(-1, 2, 2)[gait_contact==0]
     height_map_2d_grad = get_ground_gradient(height_map_2d)
     height, width = height_map_2d_grad.shape[1:3]
     
     # get neighboring 3x3 grid with center being projected foot placement
     resolution = 0.1
-    row_index = (-(foot_placement[:, 1]//resolution).long() + int((height-1)/2)).long()
-    row_index = torch.clamp(row_index, 0, height-1).unsqueeze(1).unsqueeze(2).repeat(1, 3, 3)
-    col_index = ((foot_placement[:, 0]//resolution).long() + int((width-1)/2)).long()
-    col_index = torch.clamp(col_index, 0, width-1).unsqueeze(1).unsqueeze(2).repeat(1, 3, 3)
+    row_index = (-(foot_placement[:, 1]//resolution).long() + int((height-1)/2)).long().clamp(0, height-1)
+    col_index = ((foot_placement[:, 0]//resolution).long() + int((width-1)/2)).long().clamp(0, width-1)
+    row_indexes = torch.zeros(num_envs, 3).long()
+    row_indexes[:, 0] = row_index - 1
+    row_indexes[:, 1] = row_index
+    row_indexes[:, 2] = row_index + 1
+    col_indexes = torch.zeros(num_envs, 3).long()
+    col_indexes[:, 0] = col_index - 1
+    col_indexes[:, 1] = col_index
+    col_indexes[:, 2] = col_index + 1
     
-    row_index[:, :, 0] = row_index[:, :, 1] - 1
-    row_index[:, :, 2] = row_index[:, :, 1] + 1
-    col_index[:, 0, :] = col_index[:, 1, :] - 1
-    col_index[:, 2, :] = col_index[:, 1, :] + 1
-    
-    indices = (width*col_index.view(num_envs, -1) + row_index.view(num_envs, -1)).long() # (num_envs, 9)
+    # create indices for 3x3 grid
+    indices = torch.zeros(num_envs, 9).long()
+    indices[:, 0] = row_indexes[:, 0] * width + col_indexes[:, 0]
+    indices[:, 1] = row_indexes[:, 0] * width + col_indexes[:, 1]
+    indices[:, 2] = row_indexes[:, 0] * width + col_indexes[:, 2]
+    indices[:, 3] = row_indexes[:, 1] * width + col_indexes[:, 0]
+    indices[:, 4] = row_indexes[:, 1] * width + col_indexes[:, 1]
+    indices[:, 5] = row_indexes[:, 1] * width + col_indexes[:, 2]
+    indices[:, 6] = row_indexes[:, 2] * width + col_indexes[:, 0]
+    indices[:, 7] = row_indexes[:, 2] * width + col_indexes[:, 1]
+    indices[:, 8] = row_indexes[:, 2] * width + col_indexes[:, 2]
     indices = torch.clamp(indices, 0, height*width-1).view(-1)
     
     env_ids = torch.arange(num_envs).view(-1, 1).repeat(1,9).view(-1)
     ground_gradient = height_map_2d_grad.reshape(num_envs, -1).repeat(9, 1)[env_ids, indices] # type: ignore
     ground_gradient_at_foot = ground_gradient.view(num_envs, 9).mean(dim=1)
-    
     return ground_gradient_at_foot
 
 